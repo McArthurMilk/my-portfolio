df1d7ecbf809d620defcd4d40a2ac8596bbf5b17
Luis <lmgutierrez1966@gmail.com>
2025-11-13 22:12:01 -0500

    Added TV model

--- src/ThreeScene.js
+++ src/ThreeScene.js
@@ -1,20 +1,47 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
+
+export function TVModel() {
+  const model = useGLTF("/Models/tv.glb");
+  return (
+    <primitive
+      object={model.scene}
+      position={[25, 0, -3]}  // X, Y, Z
+      //rotation={[0, Math.PI / 4, 0]} // rotate 45° on Y axis
+      scale={1.5}  // make it bigger or smaller
+      
+    />
+  );
+}
+
+loader.load("/models/tv.glb", (gltf) => {
+  const tv = gltf.scene;
+
+  tv.traverse((child) => {
+    if (child.isMesh && child.name.toLowerCase().includes("floor")) {
+      child.visible = false;   // hide it
+    }
+  });
+
+  scene.add(tv);
+});
+
+
 
 export default function ThreeScene() {
   return (
-    <Canvas style={{ width: "100vw", height: "100vh" }}>
-      <ambientLight intensity={1} />
-      <pointLight position={[10, 10, 10]} />
+    <div className="canvas-background">
+      <Canvas
+        camera={{ position: [0, 20, 50] }}  // locked camera position
+      >
+        <ambientLight intensity={1} />
+        <pointLight position={[10, 10, 10]} />
 
-      {/* Simple 3D cube */}
-      <mesh rotation={[0.4, 0.2, 0.2]}>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial color="skyblue" />
-      </mesh>
-
-      <OrbitControls />
-    </Canvas>
+        <TVModel />
+      </Canvas>
+    </div>
   );
 }
+
